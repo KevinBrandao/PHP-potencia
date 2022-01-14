ac49828038ef66380d80cdfb0adc7877ac14a1b2
KevinBrandao <kevenbrandaoms@outlook.com>
2022-01-13 22:03:08 -0300

    php-potencia

---
+++ potencia.php
@@ -0,0 +1,30 @@
+<?php
+
+funcion pot($x, $y) {
+
+    //verificar se é inteiro
+    if (!is_int($y)) {
+        return 'Numero não é inteiro';
+    }
+}
+    //verificar se é negativo
+    if ($y < 0) {
+        $x = 1 /$x;
+        $y = $y * -1;
+    }
+    $total = 1;
+
+    for($i = 0; $i < $y; $i++){
+        $total = $total * $x;
+    }
+    return $total;
+
+}
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+
+$pot = pot($num1, $num2);
+
+echo $pot;
+
+?>
